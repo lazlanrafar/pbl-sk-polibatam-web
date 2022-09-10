87d8af2c3c212cc258866535768706603a668597
lazlanrafar <lazlanrafar@gmail.com>
2022-09-11 06:43:40 +0700

    add and setup template

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,41 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div>
+    <app-topbar />
+    <app-sidebar />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <div class="app-content content">
+      <div class="content-overlay"></div>
+      <div class="header-navbar-shadow"></div>
+      <div class="content-wrapper">
+        <div class="content-header row"></div>
+        <div class="content-body">
+          <v-app></v-app>
+        </div>
       </div>
+    </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+    <app-footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import AppFooter from "./components/app/appFooter.vue";
+import AppSidebar from "./components/app/appSidebar.vue";
+import AppTopbar from "./components/app/appTopbar.vue";
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  name: "App",
+  components: { AppSidebar, AppTopbar, AppFooter },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+.v-application--wrap {
+  background: transparent;
+  min-height: 0;
+}
+</style>
---
+++ src/components/app/appSidebar.vue
@@ -0,0 +1,86 @@
+<template>
+  <div
+    class="main-menu menu-fixed menu-light menu-accordion menu-shadow"
+    data-scroll-to-active="true"
+  >
+    <div class="navbar-header">
+      <ul class="nav navbar-nav flex-row">
+        <li class="nav-item mr-auto">
+          <a class="navbar-brand" href="#">
+            <div class="brand-logo"></div>
+            <h2 class="brand-text mb-0">Vuexy</h2>
+          </a>
+        </li>
+        <li class="nav-item nav-toggle">
+          <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"
+            ><i
+              class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"
+            ></i
+            ><i
+              class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary"
+              data-ticon="icon-disc"
+            ></i
+          ></a>
+        </li>
+      </ul>
+    </div>
+    <div class="shadow-bottom"></div>
+    <div class="main-menu-content">
+      <ul
+        class="navigation navigation-main"
+        id="main-menu-navigation"
+        data-menu="menu-navigation"
+      >
+        <li class="nav-item" v-for="(item, i) in listSidebar" :key="i">
+          <a :href="item.link"
+            ><i :class="item.icon"></i
+            ><span class="menu-title">{{ item.name }}</span></a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSidebar",
+  data() {
+    return {
+      listSidebar: [
+        {
+          name: "Dashboard",
+          icon: "feather icon-home",
+          link: "",
+        },
+        {
+          name: "Tugas",
+          icon: "feather icon-book",
+          link: "",
+        },
+        {
+          name: "Keperluan",
+          icon: "feather icon-mail",
+          link: "",
+        },
+        {
+          name: "Document Grup",
+          icon: "feather icon-shopping-bag",
+          link: "",
+        },
+        {
+          name: "Manajemen User",
+          icon: "feather icon-users",
+          link: "",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.menu-title {
+  font-size: 1rem;
+}
+</style>
