dce290fc186e352bc15a69047fa3b99ae1bd676e
lazlanrafar <lazlanrafar@gmail.com>
2022-09-14 15:09:43 +0700

    fix card

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,33 +1,30 @@
 <template>
-  <section id="dashboard-analytics">
-    <div class="row">
-      <div class="col-12">
-        <div class="card">
-          <div class="card-body">
-            <h3>Manajemen User</h3>
-            <ul class="nav pl-0">
-              <li class="nav-item" v-for="item in listTab" :key="item">
-                <a
-                  :class="
-                    tabActive == item
-                      ? 'nav-link font-weight-bold pl-0 mr-3 active'
-                      : 'nav-link pl-0 mr-3'
-                  "
-                  @click="tabActive = item"
-                  href="#"
-                  >{{ item }}</a
-                >
-              </li>
-            </ul>
-            <hr />
+  <div class="row">
+    <div class="col-12">
+      <v-card class="card h-100">
+        <div class="card-body">
+          <h3>Manajemen User</h3>
+          <ul class="nav pl-0">
+            <li class="nav-item" v-for="item in listTab" :key="item">
+              <a
+                :class="
+                  tabActive == item
+                    ? 'nav-link font-weight-bold pl-0 mr-3 active'
+                    : 'nav-link pl-0 mr-3'
+                "
+                @click="tabActive = item"
+                >{{ item }}</a
+              >
+            </li>
+          </ul>
+          <hr />
 
-            <Mahasiswa v-if="tabActive == 'Mahasiswa'" />
-            <Dosen v-if="tabActive == 'Dosen'" />
-          </div>
+          <Mahasiswa v-if="tabActive == 'Mahasiswa'" />
+          <Dosen v-if="tabActive == 'Dosen'" />
         </div>
-      </div>
+      </v-card>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
