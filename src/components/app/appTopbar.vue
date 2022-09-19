a0be93a979ee4652a07798a03ac6e36c9497c790
lazlanrafar <lazlanrafar@gmail.com>
2022-09-19 13:49:45 +0700

    change theme dashboard

--- src/components/app/appTopbar.vue
+++ src/components/app/appTopbar.vue
@@ -1,51 +1,36 @@
 <template>
-  <nav
-    class="header-navbar navbar-expand-lg navbar navbar-with-menu floating-nav navbar-light navbar-shadow"
-  >
-    <div class="navbar-wrapper">
-      <div class="navbar-container content">
-        <div class="navbar-collapse" id="navbar-mobile">
-          <div
-            class="mr-auto float-left bookmark-wrapper d-flex align-items-center"
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"
+          ><i class="fas fa-bars"></i
+        ></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown user user-menu">
+        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+            class="user-image img-circle"
+            alt="User Image"
+          />
+          <span class="hidden-xs" style="color: black">{{ user.name }}</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <button
+            type="submit"
+            class="dropdown-item text-danger"
+            @click="handleLogout"
           >
-            <ul class="nav navbar-nav">
-              <li class="nav-item mobile-menu d-xl-none mr-auto">
-                <a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"
-                  ><i class="ficon feather icon-menu"></i
-                ></a>
-              </li>
-            </ul>
-          </div>
-          <ul class="nav navbar-nav float-right">
-            <li class="dropdown dropdown-user nav-item">
-              <a
-                class="dropdown-toggle nav-link dropdown-user-link"
-                href="#"
-                data-toggle="dropdown"
-              >
-                <div class="user-nav d-sm-flex d-none">
-                  <span class="user-name text-bold-600">{{ user.name }}</span
-                  ><span class="user-status">{{ user.jabatan }}</span>
-                </div>
-                <span
-                  ><img
-                    class="round"
-                    src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                    alt="avatar"
-                    height="40"
-                    width="40"
-                /></span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right">
-                <button class="dropdown-item" @click="handleLogout">
-                  <i class="feather icon-power"></i> Logout
-                </button>
-              </div>
-            </li>
-          </ul>
+            <i class="fas fa-sign-out-alt mr-2"></i> Logout
+          </button>
         </div>
-      </div>
-    </div>
+      </li>
+    </ul>
   </nav>
 </template>
 
