59fa110f68b1dd5221a8bb49ad9b04b4fdcd55d6
lazlanrafar <lazlanrafar@gmail.com>
2022-10-11 11:48:09 +0700

    install pwa

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from "./plugins/vuetify";
 import store from "./store";
 import router from "./router";
 import JsonExcel from "vue-json-excel";
+import './registerServiceWorker'
 
 Vue.component("downloadExcel", JsonExcel);
 Vue.config.productionTip = false;
