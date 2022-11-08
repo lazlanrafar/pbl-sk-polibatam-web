57183d02275ac0dd75cd0be740501cf6d8c6fc81
lazlanrafar <lazlanrafar@gmail.com>
2022-11-08 11:04:19 +0700

    change theme taggorup

--- src/views/TagGroup/index.vue
+++ src/views/TagGroup/index.vue
@@ -1,78 +1,74 @@
 <template>
   <div>
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Tag Group</h1>
+    <div class="content-header row">
+      <div class="content-header-left col-md-9 col-10 mb-5">
+        <div class="row breadcrumbs-top">
+          <div class="col-12">
+            <h2 class="content-header-title float-left mb-0">Dokumen Tugas</h2>
           </div>
         </div>
       </div>
     </div>
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <button class="btn btn-primary" @click="handleModalForm">
-                  <i class="fa fa-plus"></i>
-                  Tambah
-                </button>
-                <div class="row justify-content-end">
-                  <div class="col-md-3">
-                    <v-text-field
-                      label="Cari"
-                      prepend-inner-icon="mdi-magnify"
-                      v-model="optionsTableTagGroup.search"
-                      outlined
-                      dense
-                    />
-                  </div>
-                </div>
-                <v-data-table
-                  :headers="headers"
-                  :items="reports"
-                  :search="optionsTableTagGroup.search"
-                  :loading="isLoading"
-                  :single-expand="singleExpand"
-                  :expanded.sync="expanded"
-                  show-expand
-                >
-                  <template v-slot:[`item.no`]="props">
-                    {{ (props.index += 1) }}
-                  </template>
-                  <template v-slot:[`item.aksi`]="{ item }">
-                    <v-btn class="mr-2" icon @click="handleUpdate(item.id)">
-                      <v-icon>mdi-pencil</v-icon>
-                    </v-btn>
-                    <v-btn icon @click="handleDelete(item.id)">
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                  </template>
-                  <template v-slot:expanded-item="{ headers, item }">
-                    <td :colspan="headers.length" class="collapsable">
-                      <table class="table w-100">
-                        <tr>
-                          <th>No</th>
-                          <th>Nama</th>
-                          <th>Username</th>
-                          <th>NIM</th>
-                          <th>Status</th>
-                        </tr>
-                        <tr v-for="(x, i) in item.tag" :key="i">
-                          <td>{{ i + 1 }}</td>
-                          <td>{{ x.name }}</td>
-                          <td>{{ x.username }}</td>
-                          <td>{{ x.nim_nik_unit }}</td>
-                          <td>{{ x.jabatan }}</td>
-                        </tr>
-                      </table>
-                    </td>
-                  </template>
-                </v-data-table>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <button class="btn btn-primary" @click="handleModalForm">
+              <i class="fa fa-plus"></i>
+              Tambah
+            </button>
+            <div class="row justify-content-end">
+              <div class="col-md-3">
+                <v-text-field
+                  label="Cari"
+                  prepend-inner-icon="mdi-magnify"
+                  v-model="optionsTableTagGroup.search"
+                  outlined
+                  dense
+                />
               </div>
             </div>
+            <v-data-table
+              :headers="headers"
+              :items="reports"
+              :search="optionsTableTagGroup.search"
+              :loading="isLoading"
+              :single-expand="singleExpand"
+              :expanded.sync="expanded"
+              show-expand
+            >
+              <template v-slot:[`item.no`]="props">
+                {{ (props.index += 1) }}
+              </template>
+              <template v-slot:[`item.aksi`]="{ item }">
+                <v-btn class="mr-2" icon @click="handleUpdate(item.id)">
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn icon @click="handleDelete(item.id)">
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </template>
+              <template v-slot:expanded-item="{ headers, item }">
+                <td :colspan="headers.length" class="collapsable">
+                  <table class="table w-100">
+                    <tr>
+                      <th>No</th>
+                      <th>Nama</th>
+                      <th>Username</th>
+                      <th>NIM</th>
+                      <th>Status</th>
+                    </tr>
+                    <tr v-for="(x, i) in item.tag" :key="i">
+                      <td>{{ i + 1 }}</td>
+                      <td>{{ x.name }}</td>
+                      <td>{{ x.username }}</td>
+                      <td>{{ x.nim_nik_unit }}</td>
+                      <td>{{ x.jabatan }}</td>
+                    </tr>
+                  </table>
+                </td>
+              </template>
+            </v-data-table>
           </div>
         </div>
       </div>
