4b320ceff6ff16fec8e5f4bd9bd99e8503ebc14d
lazlanrafar <lazlanrafar@gmail.com>
2022-11-08 09:54:40 +0700

    change theme login

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,59 +1,87 @@
 <template>
-  <div>
-    <div class="login-box">
-      <div class="login-logo">
-        <a href="/">
-          <img src="@/assets/login.png" width="200" alt="" />
-        </a>
-      </div>
-      <!-- /.login-logo -->
-      <div class="card">
-        <div class="card-body login-card-body">
-          <p class="login-box-msg">POLIBATAM</p>
-          <p class="login-box-msg">Sign in to start your session</p>
-
-          <form @submit.prevent="handleSubmit">
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Username"
-                :value="username"
-                @change="username = $event.target.value"
-              />
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-user"></span>
-                </div>
-              </div>
-            </div>
-            <div class="input-group mb-3">
-              <input
-                type="password"
-                class="form-control"
-                placeholder="Password"
-                :value="password"
-                @change="password = $event.target.value"
-              />
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-lock"></span>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <button
-                  type="submit"
-                  class="btn btn-primary btn-block text-white"
+  <div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper">
+      <div class="content-header row"></div>
+      <div class="content-body">
+        <section class="row flexbox-container">
+          <div class="col-xl-8 col-11 d-flex justify-content-center">
+            <div class="card bg-authentication rounded-0 mb-0">
+              <div class="row m-0">
+                <div
+                  class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0"
                 >
-                  Sign In
-                </button>
+                  <img
+                    src="/app-assets/images/pages/login.png"
+                    alt="branding logo"
+                  />
+                </div>
+                <div class="col-lg-6 col-12 p-0">
+                  <div class="card rounded-0 mb-0 px-2">
+                    <div class="card-header pb-1">
+                      <div class="card-title">
+                        <h4 class="mb-0">Login</h4>
+                      </div>
+                    </div>
+                    <p class="px-2">
+                      Welcome back, please login to your account.
+                    </p>
+                    <div class="card-content">
+                      <div class="card-body pt-1">
+                        <form @submit.prevent="handleSubmit">
+                          <fieldset
+                            class="form-label-group form-group position-relative has-icon-left"
+                          >
+                            <input
+                              type="text"
+                              class="form-control"
+                              id="user-name"
+                              placeholder="Username"
+                              :value="username"
+                              @change="username = $event.target.value"
+                              required
+                            />
+                            <div class="form-control-position">
+                              <i class="feather icon-user"></i>
+                            </div>
+                            <label for="user-name">Username</label>
+                          </fieldset>
+
+                          <fieldset
+                            class="form-label-group position-relative has-icon-left"
+                          >
+                            <input
+                              type="password"
+                              class="form-control"
+                              id="user-password"
+                              placeholder="Password"
+                              :value="password"
+                              @change="password = $event.target.value"
+                              required
+                            />
+                            <div class="form-control-position">
+                              <i class="feather icon-lock"></i>
+                            </div>
+                            <label for="user-password">Password</label>
+                          </fieldset>
+                          <button
+                            type="submit"
+                            class="btn btn-primary text-white float-right btn-inline"
+                          >
+                            Login
+                          </button>
+                          <br />
+                          <br />
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <!-- /.col -->
             </div>
-          </form>
-        </div>
+          </div>
+        </section>
       </div>
     </div>
   </div>
@@ -92,8 +120,10 @@ export default {
     },
   },
   mounted() {
-    document.body.classList.add("login-page");
-    document.body.classList.add("hold-transition");
+    document.body.classList.add("bg-full-screen-image", "blank-page");
+  },
+  destroyed() {
+    document.body.classList.remove("bg-full-screen-image", "blank-page");
   },
 };
 </script>
