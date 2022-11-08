253badfd238d07bfdfc9a004f268de2a50653dbc
lazlanrafar <lazlanrafar@gmail.com>
2022-11-08 11:30:43 +0700

    change logo

--- src/components/app/appSidebar.vue
+++ src/components/app/appSidebar.vue
@@ -1,41 +1,5 @@
 <template>
   <div>
-    <!-- <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <router-link to="/" class="brand-link">
-        <img
-          src="@/assets/logo.svg"
-          alt="Logo"
-          class="brand-image"
-          style="opacity: 0.8"
-        />
-        <span class="brand-text font-weight-light">SK TUGAS</span>
-      </router-link>
-
-      <div class="sidebar">
-        <nav class="mt-2">
-          <ul
-            class="nav nav-pills nav-sidebar flex-column"
-            data-widget="treeview"
-            role="menu"
-            data-accordion="false"
-          >
-            <li
-              class="nav-item"
-              v-for="(item, index) in listSidebar"
-              :key="index"
-            >
-              <router-link
-                :to="item.link"
-                :class="tabActive == item.name ? 'nav-link active' : 'nav-link'"
-              >
-                <i :class="item.icon"></i>
-                <p>{{ item.name }}</p>
-              </router-link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </aside> -->
     <div
       class="main-menu menu-fixed menu-dark menu-accordion menu-shadow"
       data-scroll-to-active="true"
@@ -44,19 +8,21 @@
         <ul class="nav navbar-nav flex-row">
           <li class="nav-item mr-auto">
             <router-link class="navbar-brand" to="/">
-              <div class="brand-logo"></div>
-              <h2 class="brand-text mb-0">Vuexy</h2>
+              <img src="@/assets/logo.webp" width="40" class="" />
+              <h2 class="brand-text mb-0" style="color: #65b7de">Pigeon</h2>
             </router-link>
           </li>
           <li class="nav-item nav-toggle">
-            <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"
-              ><i
+            <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
+              <i
                 class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"
-              ></i
-              ><i
+              >
+              </i>
+              <i
                 class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary"
                 data-ticon="icon-disc"
-              ></i
+              >
+              </i
             ></a>
           </li>
         </ul>
